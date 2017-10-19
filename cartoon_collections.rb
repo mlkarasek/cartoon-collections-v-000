9122a0f7b16ca7c9768c9ea1b361ea379ee5d4ba
Monica  Karasek <mlkarasek@gmail.com>
2017-10-19 20:03:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long, calls_short)
+
 end
 
 def find_the_cheese# code an argument here
